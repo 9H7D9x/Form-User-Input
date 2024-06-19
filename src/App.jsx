b58e6cc7445aa3637229c4da62a2370b8c504cf8
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-06-19 17:55:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+import SignUp from './components/SignUp.jsx';
+import SubmitLogin from './components/SubmitLogin.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <SignUp />
+      </main>
+    </>
+  );
+}
+
+export default App;
