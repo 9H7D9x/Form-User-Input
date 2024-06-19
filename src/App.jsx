8ce10d8adeafddd706e16863ad3bd4ee68910a5e
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-06-19 21:20:50 +0530

    reseting part and formData object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header.jsx';
 import SignUp from './components/SignUp.jsx';
-import SubmitLogin from './components/SubmitLogin.jsx'
+// import SubmitLogin from './components/SubmitLogin.jsx'
 
 function App() {
   return (
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,10 @@ export default function Login() {
   const handleSubmit=(event)=>{
     event.preventDefault();
     console.log(enteredValues);
+    setEnteredValues({
+      email:'',
+      password:'',
+    })
   }
 
   const handleInputValues=(identifier , value)=>{
